aa1d987b35e67193265ed60f27cab849ee5531c7
TakoLukava <34337042+TakoLukava@users.noreply.github.com>
2018-02-26 09:41:28 +0300

    Update hw5_wk3.py

--- hw5_wk3.py
+++ hw5_wk3.py
@@ -1,23 +1,13 @@
-from random import random
-N=10
-a=[10,13,0,23,40,12]
-l = int(input('Enter min number of strings: '))
-h = int(input('enter max number of strings:  '))
+i = 0
+n = input('Enter the range of numbers: ')
+_list = list(n)
+ln = len(_list)
 
-for i in range(N):
-    n=int(random()*(h-1)) + 1
-    a.append(n)
-print(a)
 
-v=int(0|10)
-i=0
-m=N
-while i < m:
-    if a[i] == v:
-        del a[i]
-        m -= 1
-    else:
-        i += 1
-for i in range(m,N):
-    a.append(-1)
-print(a)
+while i <= ln-1:
+   if _list[i] == '0':
+       _list.remove('0')
+       _list.append(-1)
+       i -= 1
+   i += 1
+print(_list)
